8da7205353c285bc2f84fcde5ee64644da5487cb
Alberto Miranda <allowski@gmail.com>
2015-09-17 07:21:23 -0400

    1442488883

--- js/a4pp.js
+++ js/a4pp.js
@@ -1055,3 +1055,26 @@ function a4pp_download_file(url, fname, prog){
     function fail(evt) {
         alert(evt.target.error.code);
     }
+    
+    function getpos(){
+
+		var onSuccess = function(position) {
+			alert('Latitude: '          + position.coords.latitude          + '\n' +
+				  'Longitude: '         + position.coords.longitude         + '\n' +
+				  'Altitude: '          + position.coords.altitude          + '\n' +
+				  'Accuracy: '          + position.coords.accuracy          + '\n' +
+				  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+				  'Heading: '           + position.coords.heading           + '\n' +
+				  'Speed: '             + position.coords.speed             + '\n' +
+				  'Timestamp: '         + position.timestamp                + '\n');
+		};
+
+		// onError Callback receives a PositionError object
+		//
+		function onError(error) {
+			alert('code: '    + error.code    + '\n' +
+				  'message: ' + error.message + '\n');
+		}
+
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	}
