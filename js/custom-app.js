12a219a6efa9496d151e1c96bd6de44dc9669af5
Alberto Miranda <allowski@gmail.com>
2015-09-24 15:28:51 -0400

    1443122931

--- js/custom-app.js
+++ js/custom-app.js
@@ -1,9 +1,9 @@
-var map = null;
-$(document).on("deviceready", function() {
-  map = plugin.google.maps.Map.getMap();
-  map.on(plugin.google.maps.event.MAP_READY, onMapReady);
-});
-
-function onMapReady() {
-  alert("mapready");
+function downloadF(){
+	
+	var url = "http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml:";
+	
+	var name = "cta.kml";
+	
+	downloadFile(url, name, "");
+	
 }
