a92c1c5db4a4ffb7b3b09906d3acbe65803b8536
Alberto Miranda <allowski@gmail.com>
2015-09-25 08:25:42 -0400

    1443183942

--- js/custom-app.js
+++ js/custom-app.js
@@ -3,45 +3,47 @@ function register_position(){
 	toast("<span class='spinner glyphicon glyphicon-refresh'></span> "+i("Aguarde..", "Aguarde..", "Wait.."), "warning", 50000);
 	
 	$("#btn").attr("src", "img/ponto-alt.png");
-	
-	getpos(function(res){
-		
-		var ps = JSON.parse(window.localStorage["pos"]);
-		
-		var nw  = {};
-		
-		nw.accuracy = res.accurancy;
-		nw.altitude = res.altitude;
-		nw.altitudeAccuracy = res.altitudeAccuracy;
-		nw.heading = res.heading;
-		nw.latitude = res.latitude;
-		nw.longitude = res.longitude;
-		nw.speed = res.speed;
-		
-		ps.push(nw);
-		
-		window.localStorage["pos"] = JSON.stringify(ps);
-		
-		toast(i("Capturado", "Posicion capturada", "Captured position!"), "success", 3000);
-		
-		$("#btn").attr("src", "img/ponto.png");
-		
-		setTimeout(function(){
-		
-			if(isConnected()){
-				
-				sendAll();
-				
-			}else{
-				
-				toast("offline", "warning", 1000);
-				
-			}
+	setTimeout(function(){
+		getpos(function(res){
 			
-		}, 3000);
+			var ps = JSON.parse(window.localStorage["pos"]);
+			
+			var nw  = {};
+			
+			nw.accuracy = res.accurancy;
+			nw.altitude = res.altitude;
+			nw.altitudeAccuracy = res.altitudeAccuracy;
+			nw.heading = res.heading;
+			nw.latitude = res.latitude;
+			nw.longitude = res.longitude;
+			nw.speed = res.speed;
+			
+			ps.push(nw);
+			
+			window.localStorage["pos"] = JSON.stringify(ps);
+			
+			toast(i("Capturado", "Posicion capturada", "Captured position!"), "success", 3000);
+			
+			$("#btn").attr("src", "img/ponto.png");
+			
+			setTimeout(function(){
+			
+				if(isConnected()){
+					
+					sendAll();
+					
+				}else{
+					
+					toast("offline", "warning", 1000);
+					
+				}
+				
+			}, 3000);
+			
+			
+		});
 		
-		
-	});
+	},1500);
 	
 }
 
