e14d8cb1947a5af44a1a91cc1f100c73147fd09e
Alberto Miranda <allowski@gmail.com>
2015-10-08 14:54:37 -0400

    1444330477

---
+++ js/forms.js
@@ -0,0 +1,15 @@
+function get_data(){
+	
+	toast("Sincronizando..", "Sincronizando..", "Syncing..", "success");
+	
+	for(var i  in remember.collections){
+	
+		$.post(window.app.update_url, {"action":"get_data", "collection":i}, function(){
+			
+		
+		});
+	
+	}
+	
+	
+}
