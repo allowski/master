7c3e216fba48d0e9e1d605bd8e0db4c1f7a214e5
Alberto Miranda <allowski@gmail.com>
2015-10-08 15:36:05 -0400

    1444332965

--- js/forms.js
+++ js/forms.js
@@ -21,35 +21,37 @@ function get_data(){
 	
 	iv = setInterval(function(){ 
 		
-		if(!list_col[c]) return;
-		if(list_col[c]=="undefined") return;
-		if(list_col[c]=="collections") return;
+		if((!list_col[c])&&(list_col[c]=="undefined")&&(list_col[c]=="collections"){
+			
+		}else{
 		
-		if(c>=list_col.length){
-			console.log("Fim");
-			toast(t("Sincronizacion terminada ", "Sincronizacao terminada", "Syncing finished"), "success", 3000);
-			clearInterval(iv);
+			if(c<list_col.length){
+				console.log("Fim");
+				toast(t("Sincronizacion terminada ", "Sincronizacao terminada", "Syncing finished"), "success", 3000);
+				clearInterval(iv);
+			}
+			
+			if(processing == 1){ 
+				
+				return;
+			
+			}
+			
+			toast(t("Sincronizando "+list_col[c], "Sincronizando "+list_col[c], "Syncing.. "+list_col[c]), "warning", 50000);
+			
+			processing = 1;
+			
+			console.log("Syncing: "+list_col[c]);
+			
+			$.post(window.app.update_url, {"action":"get_data", "collection":i}, function(r){
+				
+				processing = 0;
+				
+				remember.collections[i] = r.data;
+			
+			});
+			
 		}
-		
-		if(processing == 1){ 
-			
-			return;
-		
-		}
-		
-		toast(t("Sincronizando "+list_col[c], "Sincronizando "+list_col[c], "Syncing.. "+list_col[c]), "warning", 50000);
-		
-		processing = 1;
-		
-		console.log("Syncing: "+list_col[c]);
-		
-		$.post(window.app.update_url, {"action":"get_data", "collection":i}, function(r){
-			
-			processing = 0;
-			
-			remember.collections[i] = r.data;
-		
-		});
 		
 		c++;
 	
