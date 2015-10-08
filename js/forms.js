a51ca53e3ade834483fefa4cdb48aabf378bb661
Alberto Miranda <allowski@gmail.com>
2015-10-08 15:40:08 -0400

    1444333208

--- js/forms.js
+++ js/forms.js
@@ -21,15 +21,16 @@ function get_data(){
 	
 	iv = setInterval(function(){ 
 		
+	
+		if(c>=list_col.length){
+			console.log("Fim");
+			toast(t("Sincronizacion terminada ", "Sincronizacao terminada", "Syncing finished"), "success", 3000);
+			clearInterval(iv);
+		}
+		
 		if((!list_col[c])||(list_col[c]=="undefined")||(list_col[c]=="collections")){
 			
 		}else{
-		
-			if(c<list_col.length){
-				console.log("Fim");
-				toast(t("Sincronizacion terminada ", "Sincronizacao terminada", "Syncing finished"), "success", 3000);
-				clearInterval(iv);
-			}
 			
 			if(processing == 1){ 
 				
