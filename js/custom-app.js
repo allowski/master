6b2fbcb8cedb70005e90e548a5acc3444e17d9b1
Alberto Miranda <allowski@gmail.com>
2015-10-22 13:31:46 -0400

    1445535106

--- js/custom-app.js
+++ js/custom-app.js
@@ -1,7 +1,11 @@
 function filterClients(val){
 	if(val!=""){
 		$("#clis").find("div.cli").removeClass("hidden");
-		$("#clis").find("div.cli:not(:contains('"+val+"'))").addClass("hidden");
+		$("#clis").find("div.cli:not(:Contains('"+val+"'))").addClass("hidden");
 	}
 	
 }
+
+jQuery.expr[':'].Contains = function(a,i,m){
+    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+};
