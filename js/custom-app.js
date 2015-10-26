6628f6e2ff3578d97593b392f0d11f3341f90ae0
Alberto Miranda <allowski@gmail.com>
2015-10-26 15:42:50 -0400

    1445888570

--- js/custom-app.js
+++ js/custom-app.js
@@ -11,3 +11,14 @@ function filterClients(val){
 jQuery.expr[':'].Contains = function(a,i,m){
     return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
 };
+
+
+function getEleClass(dd){
+	if(dd.area_total != '' || dd.area_propia != '' 
+	|| dd.area_arrendada !='' || dd.costo_alquiler !='' || dd.estimativa_produccion_1 !=''
+	|| dd.imagen_1 != '' || dd.punto_gps != ''){
+		return "ok";
+	}else{
+		return "not-ok";
+	}
+}
