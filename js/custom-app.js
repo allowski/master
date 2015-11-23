88712d55319ac2556df28f15e2f5fd0973346b4b
Alberto Miranda <allowski@gmail.com>
2015-11-23 11:46:01 -0500

    1448297161

--- js/custom-app.js
+++ js/custom-app.js
@@ -2,7 +2,16 @@
  * 
  * 	CostoBeneficio App
  * 
- */ 
+ */
+ 
+ 
+String.prototype.insert = function (index, string) {
+  if (index > 0)
+    return this.substring(0, index) + string + this.substring(index, this.length);
+  else
+    return string + this;
+}; 
+  
 
 var costoBeneficio = {
 	"files": {},
