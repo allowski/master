4ad85464b6cb86233dd90243e1c3a22e7cc7729d
Alberto Miranda <allowski@gmail.com>
2015-11-24 13:53:34 -0500

    1448391214

--- js/custom-app.js
+++ js/custom-app.js
@@ -84,7 +84,7 @@ var costoBeneficio = {
 		
 		$.each(this.files, function(){
 			
-			costoBeneficio.fileList.append("<a class='list-group-item text-uppercase' onclick='costoBeneficio.editFile(event, \""+this.filename+"\");'>"+this.filename+" ---> ("+this.ton+") <small class='pull-right'>["+this.datetime+"]</small></a>");
+			costoBeneficio.fileList.append("<a class='list-group-item text-uppercase' onclick='costoBeneficio.editFile(event, \""+this.filename+"\");'>"+this.filename+" <small class='pull-right'>["+this.datetime+"]</small></a>");
 			
 		});
 		
