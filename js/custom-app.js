d15b65a6e65ffa19195f24ffceda8e7f4c0ac248
Alberto Miranda <allowski@gmail.com>
2015-12-14 13:42:22 -0500

    1450118542

--- js/custom-app.js
+++ js/custom-app.js
@@ -3,7 +3,27 @@ var App = {
 	
 	"afterRenderMain" : function(){
 		
-		$("#qtde").text(remember.collections.form_12.length);
+		$("#qtde").text(this.getTodayVisitCount());
+		
+	}
+	
+	"getTodayVisitCount": function(){
+		
+		var retVal = 0;
+		
+		for(x in remember.collections.form_12){
+			
+			var currentRow = remember.collections.form_12[x];
+			
+			if(currentRow.control3 == "2015-12-14"){
+				
+				retVal+=1;
+				
+			}
+			
+		}
+		
+		return retVal;
 		
 	}
 	
