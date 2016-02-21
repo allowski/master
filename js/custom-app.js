0a63550928e30250317d6199b4e41026668d433b
Alberto Miranda <allowski@gmail.com>
2016-02-20 23:04:21 -0500

    {"appId":"54","shortcut":"build-monitor","pgBuildId":"1583648"}

--- js/custom-app.js
+++ js/custom-app.js
@@ -4,7 +4,17 @@ window.setTimeout(function(){
 
 	window.socket.on("update_builds", function(updates){
 		
-		console.log(updates);
+		for(build in updates){
+			
+			var curr_build  = updates[build];
+			
+			var new_color = "#333";
+			
+			new_color = (curr_build.isFree == "Y") ? '#090' : '#d00';
+			
+			$("#b"+curr_build.app_id).css("color", new_color);
+			
+		}
 		
 	});
 
