ef17667501c6391faf067f731bdddc59beb2d3ac
Alberto Miranda <allowski@gmail.com>
2016-02-20 22:53:50 -0500

    {"appId":"54","shortcut":"build-monitor","pgBuildId":"1583648"}

--- js/custom-app.js
+++ js/custom-app.js
@@ -1,7 +1,11 @@
-window.socket.emit("subscribe_to_builds");
+window.setTimeout(function(){
+	
+	window.socket.send("subscribe_to_builds");
 
-window.socket.on("update_builds", function(updates){
-	
-	console.log(updates);
-	
-});
+	window.socket.on("update_builds", function(updates){
+		
+		console.log(updates);
+		
+	});
+
+}, 2000);
