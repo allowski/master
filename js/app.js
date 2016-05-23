96aa0b0d96b085e4d279738190fb9d0c1da863f8
Alberto Miranda <allowski@gmail.com>
2016-05-23 07:41:41 -0400

    {"appId":"81","shortcut":"auto","pgBuildId":"1656033","datetime":"2016-05-23 12:41:41"}

--- js/app.js
+++ js/app.js
@@ -16,105 +16,78 @@ function main(){
 
 
 
-setTimeout(function(){
-	main();
-	setTimeout(function(){
-		if(window.app.logged==true){
-			a4pp_update();
-		}
+	
+main();
+
+if(isPhoneGap()){
 		
-		if(isPhoneGap()){
+	document.addEventListener('deviceready', function () {
+		
+		var fail = failCB('requestFileSystem');
+		var tp = window.PERSISTENT;
+		if(typeof LocalFileSystem != "undefined"){
+			tp = LocalFileSystem.PERSISTENT;
+		}
+		window.requestFileSystem(tp, 0, gotFS, fail);
+		
+		if(("plugins" in window)&&(!window.app.logged_in)){
 			
-			setTimeout(function(){
-				document.addEventListener('deviceready', function () {
-					
-					
-					var myFile = new FileManager(".cloudcrm/test.txt", function(){ 
-						
-						myFile.write("CCRM: test test test", function(){
-							
-							myFile.read(function(text){
-							
-								console.log(text);
-							
-							});
-							
-						});
-					
-					});
-					
-					return;
-					
-					var fail = failCB('requestFileSystem');
-					var tp = window.PERSISTENT;
-					if(typeof LocalFileSystem != "undefined"){
-						tp = LocalFileSystem.PERSISTENT;
-					}
-					window.requestFileSystem(tp, 0, gotFS, fail);
-					
-					if(("plugins" in window)&&(!window.app.logged_in)){
-						
-						
-						try{		
-							
-							window.plugins.webintent.getUri(function(url) {
-								if((url !== "")&&(url!=null)) {
-									console.log("Current Token Is: "+url);
-									window.localStorage["userTokenSafe"] = url.split(':')[1];
-									console.log(window.localStorage["userTokenSafe"]);
-									a4pp_update({}, window.localStorage["userTokenSafe"]);
-								}else{
-									// There was no extra supplied.
-									window.plugins.webintent.startActivity(
-										{
-										  action: "app.cloudcrm.tech.cloudcrm.auth",
-										  extras: {
-											"return" : window.app.appDomain
-										  }
-										},
-										function() {
-											
-										},
-										function() {
-										  alert('Failed to open URL via Android Intent.');
-										  console.log("Failed to open URL via Android Intent. URL: " + theFile.fullPath)
-										}
-									);
-								}
-							}, function(){
-							
-									// There was no extra supplied.
-									window.plugins.webintent.startActivity(
-										{
-										  action: "app.cloudcrm.tech.cloudcrm.auth",
-										  extras: {
-											"return" : window.app.appDomain
-										  }
-										},
-										function() {
-											
-										},
-										function() {
-										  alert('Failed to open URL via Android Intent.');
-										  console.log("Failed to open URL via Android Intent. URL: " + theFile.fullPath)
-										}
-									);
-								
-							});	
-								
-							
-						}catch(err){
-
-							alert(err);
-
-						}
-					}
-					
-				}, false);
+			
+			try{		
 				
-			}, 500);
-			
+				window.plugins.webintent.getUri(function(url) {
+					if((url !== "")&&(url!=null)) {
+						console.log("Current Token Is: "+url);
+						window.localStorage["userTokenSafe"] = url.split(':')[1];
+						console.log(window.localStorage["userTokenSafe"]);
+						a4pp_update({}, window.localStorage["userTokenSafe"]);
+					}else{
+						// There was no extra supplied.
+						window.plugins.webintent.startActivity(
+							{
+							  action: "app.cloudcrm.tech.cloudcrm.auth",
+							  extras: {
+								"return" : window.app.appDomain
+							  }
+							},
+							function() {
+								
+							},
+							function() {
+							  alert('Failed to open URL via Android Intent.');
+							  console.log("Failed to open URL via Android Intent. URL: " + theFile.fullPath)
+							}
+						);
+					}
+				}, function(){
+				
+						// There was no extra supplied.
+						window.plugins.webintent.startActivity(
+							{
+							  action: "app.cloudcrm.tech.cloudcrm.auth",
+							  extras: {
+								"return" : window.app.appDomain
+							  }
+							},
+							function() {
+								
+							},
+							function() {
+							  alert('Failed to open URL via Android Intent.');
+							  console.log("Failed to open URL via Android Intent. URL: " + theFile.fullPath)
+							}
+						);
+					
+				});	
+					
+				
+			}catch(err){
+
+				alert(err);
+
+			}
 		}
 		
-	},1000);
-}, 600);
+	}, false);
+
+}
