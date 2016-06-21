65ce3ed7d3a3768632aa9ef252135ba458b0b3db
Alberto Miranda <allowski@gmail.com>
2016-06-21 08:55:13 -0400

    {"appId":"18","shortcut":"auto","pgBuildId":"1656033","datetime":"2016-06-21 13:55:13"}

---
+++ js/custom-app.js
@@ -0,0 +1,42 @@
+function filterClients(val){
+	if(val!=""){
+		$("#clis").find("div.cli").removeClass("hidden");
+		$("#clis").find("div.cli:not(:Contains('"+val+"'))").addClass("hidden");
+	}else{
+		$("#clis").find("div.cli").removeClass("hidden");
+	}
+	
+}
+
+jQuery.expr[':'].Contains = function(a,i,m){
+    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+};
+
+
+function getEleClass(dd){
+	if(dd.area_total != '' || dd.area_propia != '' 
+	|| dd.area_arrendada !='' || dd.costo_alquiler !='' || dd.estimativa_produccion_1 !=''
+	|| dd.imagen_1 != '' || dd.punto_gps != ''){
+		return "ok";
+	}else{
+		return "not-ok";
+	}
+}
+
+function sendByEmail(){
+	
+	var email = "";
+	
+	email = prompt("DIGITE EMAIL:");
+
+	if(email){
+		
+		$.get(window.app.update_url+"&action=send_email&id="+remember.collections.deudas[remember.current_item.id].id+"&ent="+$("[name=entidad_]").val()+"&email="+email, function(r){
+			//resp = JSON.parse(r);
+			alert(r.message);
+			console.log(r);
+		});
+		
+	}
+	
+}
